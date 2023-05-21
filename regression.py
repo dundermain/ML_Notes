a4a41bb161fc9f32138626aedcd03f8ca43ae31c
SynapticLad <sachin68834@gmail.com>
2023-05-21 11:58:48 +0530

    Added files

--- regression.py
+++ regression.py
@@ -3,4 +3,9 @@
 #Fiiting a regression model using SciKIt Learn
 
 import numpy as np
-from sklearn.linear_model
+from sklearn.linear_model import LinearRegression
+
+reg = LinearRegression()
+reg.fit(x_train, y)
+
+prediction_space = np.linspace()
