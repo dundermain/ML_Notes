dfb03ff41cdda78372e9a2f35af3ff230600a2cf
SynapticLad <sachin68834@gmail.com>
2023-05-21 11:57:29 +0530

    Added files

--- knn.py
+++ knn.py
@@ -1,3 +1,9 @@
+#Part of DAtacamp notes from the coursee ML with Scikit learn
+
+#Classifier models = KNN
+
+
+
 from sklearn.model_selection import train_test_split
 
 x_train, y_train, x_test, y_test = train_test_split(X, y, test_size=0.3, random_state=21, stratify=y)
---
+++ regression.py
@@ -0,0 +1,6 @@
+#Part of DAtacamp notes
+
+#Fiiting a regression model using SciKIt Learn
+
+import numpy as np
+from sklearn.linear_model
