239c83b693d68d05bfd35ac240110b66a0b78e38
SynapticLad <sachin68834@gmail.com>
2023-05-21 12:03:16 +0530

    Added files

--- knn.py
+++ knn.py
@@ -5,6 +5,7 @@
 
 
 from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
 
 x_train, y_train, x_test, y_test = train_test_split(X, y, test_size=0.3, random_state=21, stratify=y)
 #staritfy splits the data into train and test splits such that train and test claass has same proportion of class labels as input dataset.
--- regression.py
+++ regression.py
@@ -9,3 +9,16 @@ reg = LinearRegression()
 reg.fit(x_train, y)
 
 prediction_space = np.linspace()
+
+
+#Linear regression on all features
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+x_train, y_train, x_test, y_test = train_test_split(X, y, test_size=0.3, random_state=21)
+
+reg = LinearRegression()
+reg.fit(x_train, y_train)
+y_pred = reg.predict(x_test)
+reg.score(x_test, y_test)
